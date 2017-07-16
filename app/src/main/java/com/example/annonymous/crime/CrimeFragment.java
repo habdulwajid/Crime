ec152af128b02c63e9374adfcb29fb91cd6d50ca
Abdul Wajid <abdulwajid609@gmail.com>
2017-07-16 14:51:03 +0500

    added calender and custom date functionality , working fine but default date is wrong starting from 1900

--- app/src/main/java/com/example/annonymous/crime/CrimeFragment.java
+++ app/src/main/java/com/example/annonymous/crime/CrimeFragment.java
@@ -1,9 +1,12 @@
 package com.example.annonymous.crime;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
@@ -32,6 +35,9 @@ public class CrimeFragment extends Fragment {
     private ViewGroup mContainer;
     private Bundle mSavedInstanceState;
     private static final String ARG_CRIME_ID="crime_id";
+    private static final String DIALOG_DATE ="DateDialog";
+
+    private static final int REQUEST_DATE =0;
 
 
     public static CrimeFragment newInstance(UUID crimeId){
@@ -81,9 +87,22 @@ public class CrimeFragment extends Fragment {
         });
 
         mDateButton = (Button)v.findViewById(R.id.crime_date);
-        mDateButton.setText(mCrime.getDate().toString());
-     //   mDateButton.setText(mCrime.getDate().toString());
-        mDateButton.setEnabled(false);
+       updateDate();
+//        mDateButton.setText(mCrime.getDate().toString());
+//        mDateButton.setEnabled(true);
+
+        mDateButton.setOnClickListener(new  View.OnClickListener(){
+          @Override
+            public void onClick(View v){
+              FragmentManager manager = getFragmentManager();
+//              DatePickerFragment dialog = new DatePickerFragment();
+
+              DatePickerFragment dialog = DatePickerFragment
+                      .newInstance(mCrime.getDate());
+              dialog.setTargetFragment(CrimeFragment.this,REQUEST_DATE);
+              dialog.show(manager, DIALOG_DATE);
+          }
+        });
         mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
         mSolvedCheckBox.setChecked(mCrime.isSolved());
         mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
@@ -97,4 +116,24 @@ public class CrimeFragment extends Fragment {
         return  v;
 
     }
+
+    private void updateDate() {
+        mDateButton.setText(mCrime.getDate().toString());
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (resultCode != Activity.RESULT_OK){
+            return;
+        }
+        if (requestCode == REQUEST_DATE){
+            Date date = (Date)data
+                    .getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+            mCrime.setDate(date);
+           mDateButton.setText(mCrime.getDate().toString());
+            updateDate();
+        }
+    }
 }
+
+
